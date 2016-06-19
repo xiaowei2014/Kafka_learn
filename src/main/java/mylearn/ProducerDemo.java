8792abe1e922f7ac121d2a0acd16024baf2cb3b2
zhangxiaowei <353555080@qq.com>
2016-06-19 17:48:02 +0800

    demo

---
+++ src/main/java/mylearn/ProducerDemo.java
@@ -0,0 +1,30 @@
+package mylearn;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * Created by 张小伟 on 2016/6/19.
+ */
+public class ProducerDemo {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for(int i = 0; i < 100; i++)
+            producer.send(new ProducerRecord<String, String>("my-topic", Integer.toString(i), Integer.toString(i)));
+
+        producer.close();
+    }
+}
